5599a3f8a921fa57a82985eedf60d737b6dc593d
tmdwnsasa <tmdwns_as@naver.com>
2024-04-28 02:23:15 +0900

    Add files via upload
    기능 조금 추가 및 코드 정리

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+class movieManager {
+    constructor(currPage, maxPage, currMaxPage, allData, dataForShow, quantity) {
+        this._currPage = 0;
+        this._maxPage = null;
+        this._currMaxPage = 1;
+        this._allData = [];
+        this._dataForShow = [];
+        this._quantity = 2;
+    }
+
+    get currPage() {
+        return this._currPage;
+    }
+    set currPage(value) {
+        this._currPage = value;
+    }
+    get maxPage() {
+        return this._maxPage;
+    }
+    set maxPage(value) {
+        this._maxPage = value;
+    }
+    get currMaxPage() {
+        return this._currMaxPage;
+    }
+    set currMaxPage(value) {
+        if (this._currMaxPage > value)
+            return;
+
+        this._currMaxPage = value;
+    }
+    get allData() {
+        return this._allData;
+    }
+    set allData(value) {
+        this._allData = value;
+    }
+    get dataForShow() {
+        return this._dataForShow;
+    }
+    set dataForShow(value) {
+        this._dataForShow = value;
+    }
+    get quantity() {
+        return this._quantity;
+    }
+    set quantity(value) {
+        this._quantity = value;
+    }
+}
+
 class movieDatas {
     constructor(title, overview, poster_path, vote_average, id) {
         this._title = title;
@@ -52,42 +103,12 @@ class movieDatas {
                 </div>
         </div>
         `
+
         document.querySelector('#cards').insertAdjacentHTML('beforeend', carddata);
-
-        // const div = document.createElement('div');
-        // const cardname = 'card' + document.querySelector('#cards').childElementCount;
-        // div.className = 'card';
-        // div.id = cardname;
-        // document.querySelector('#cards').append(div);
-
-        // //이미지 넣기
-        // var img = document.createElement("img");
-        // img.src = `https://image.tmdb.org/t/p/w500${this._poster_path}`;
-        // div.appendChild(img);
-
-        // //카드 배경 넣기
-        // const backdiv = document.createElement('div');
-        // backdiv.className = 'card-body';
-
-        // const cardtitlediv = document.createElement('h5');
-        // cardtitlediv.className = 'card-title';
-        // cardtitlediv.innerText = `${this._title}`;
-        // backdiv.append(cardtitlediv);
-
-        // const cardoverviewdiv = document.createElement('p');
-        // cardoverviewdiv.className = 'card-overview';
-        // cardoverviewdiv.innerText = `${this._overview}`;
-        // backdiv.append(cardoverviewdiv);
-
-        // const cardvotediv = document.createElement('p');
-        // cardvotediv.className = 'card-vote';
-        // cardvotediv.innerText = `${this._vote_average}`;
-        // backdiv.append(cardvotediv);
-
-        // document.querySelector('#' + cardname).append(backdiv);
-        // console.log(typeof document.querySelector('#' + cardname))
     }
 }
+
+const MM = new movieManager();
 
 const options = {
     method: 'GET',
@@ -97,78 +118,172 @@ const options = {
     }
 };
 
-let dataSearch = [];
+//해당 페이지 api 받기
+const getData = async function (pages) {
+    if (MM.currMaxPage > pages) {
 
+        return;
+    }
+    MM.currMaxPage++;
+    const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${pages}`, options);
+    const data = await response.json();
+    MM.maxPage = data['total_pages'];
+    const dataarr = [];
+
+    for (const movie of data['results']) {
+        const temp = new movieDatas(movie['title'], movie['overview'], movie['poster_path'], movie['vote_average'], movie['id'])
+        dataarr.push(temp);
+    }
+    MM.allData.push(dataarr);
+}
+
+//출력
+const print = async (pages) => {
+
+    for (let i = 0; i < MM.quantity; i++) {
+        await getData(pages + i);
+        MM.currPage = pages + i;
+        console.log(MM.currPage);
+    }
+
+    MM.dataForShow = [];
+    for (let i = MM.currPage - MM.quantity; i < MM.currPage; i++) {
+        MM.allData[i].forEach(item => {
+            MM.dataForShow.push(item)
+        });
+    }
+    count = 0;
+    MM.dataForShow.forEach(item => {
+        item.makeCard(count);
+        count++;
+    });
+
+    await awake();
+    giveCardEvent();
+}
+
+//실행될 때 한번 실행될 코드 (이벤트 붙이기 등)
+const awake = async () => {
+
+
+    //페이지 켜질 때 검색창 이벤트
+    document.getElementById("input").focus();
+    document.getElementById("input").addEventListener('keydown', event => {
+        if (event.code == 'Enter')
+            search();
+    });
+}
+
+//카드 이벤트
+const giveCardEvent = () => {
+    for (let i = 0; i < document.getElementById('cards').childElementCount; i++) {
+        document.getElementById(`cardbutton${i}`).addEventListener('click', function (event) {
+            alert(`영화 id : ${MM.dataForShow[i]['id']}`);
+        })
+    }
+}
+
+//이전 페이지
+const beforeList = function () {
+    document.querySelector('#cards').innerHTML = '';
+    if (MM.currPage - MM.quantity * 2 < 1) {
+        MM.currPage = 1;
+    } else {
+        MM.currPage = MM.currPage - (MM.quantity * 2 - 1);
+    }
+    print(MM.currPage);
+    giveCardEvent();
+}
+
+//다음 페이지
+const nextList = function () {
+    document.querySelector('#cards').innerHTML = '';
+    if (MM.currPage < MM.maxPage - MM.quantity) {
+        MM.currPage++;
+    } else {
+        MM.currPage = MM.maxPage - MM.quantity
+    }
+    print(MM.currPage);
+    giveCardEvent();
+}
+
+//검색용 
 const search = function () {
+    document.querySelector('#cards').innerHTML = '';
     const searchedWord = document.querySelector('#input').value;
+    console.log(searchedWord);
+    if (searchedWord == '') {
+        print(MM.currPage - (MM.quantity - 1));
+        return;
+    }
     let count = 0;
-    console.log(dataSearch);
+    MM.dataForShow = [];
+    document.querySelector('#cards').innerHTML = '';
 
-    dataSearch.forEach(item => {
+    MM.allData.forEach(item => {
         item.forEach(item2 => {
             let str1 = item2.title.toUpperCase();
             let str2 = searchedWord.toUpperCase();
-            if (str1.search(str2) === -1) {
-                document.querySelector('#cards').children[count].style.display = 'none';
-            } else {
-                document.querySelector('#cards').children[count].style.display = 'block';
+            if (str1.search(str2) !== -1) {
+                MM.dataForShow.push(item2);
             }
-            count++;
         })
-    })
+    });
+
+    MM.dataForShow.forEach(item => {
+        item.makeCard(count);
+        count++;
+    });
+
+    giveCardEvent();
 }
 
+//이름정렬
 const namesort = function () {
-    for (let item of dataSearch) {
-        item.sort((a, b) => {
-            if (a['title'] < b['title'])
-                return -1;
-            if (a['title'] > b['title'])
-                return 1;
-            if (a['title'] === b['title'])
-                return 0;
-        });
-    }
+    MM.dataForShow.sort((a, b) => {
+        if (a['title'] < b['title'])
+            return -1;
+        if (a['title'] > b['title'])
+            return 1;
+        if (a['title'] === b['title'])
+            return 0;
+    });
     let count = 0;
 
     document.querySelector('#cards').innerHTML = '';
-    dataSearch.forEach(item => {
-        item.forEach(item2 => {
-            item2.makeCard(count);
-            count++;
-        })
+    MM.dataForShow.forEach(item => {
+        item.makeCard(count);
+        count++;
     });
-    awake();
+    giveCardEvent();
 }
 
+//평점 정렬
 const votesort = function () {
-    for (let item of dataSearch) {
-        item.sort((a, b) => {
-            if (a['vote_average'] < b['vote_average'])
-                return 1;
-            if (a['vote_average'] > b['vote_average'])
-                return -1;
-            if (a['vote_average'] === b['vote_average'])
-                return 0;
-        });
-    }
+    MM.dataForShow.sort((a, b) => {
+        if (a['vote_average'] < b['vote_average'])
+            return 1;
+        if (a['vote_average'] > b['vote_average'])
+            return -1;
+        if (a['vote_average'] === b['vote_average'])
+            return 0;
+    });
     let count = 0;
 
     document.querySelector('#cards').innerHTML = '';
-    dataSearch.forEach(item => {
-        item.forEach(item2 => {
-            item2.makeCard(count);
-            count++;
-        })
+    MM.dataForShow.forEach(item => {
+        item.makeCard(count);
+        count++;
     });
-    awake();
+    giveCardEvent();
 }
 
-const vote85 = () => {
+//평점 8.35점 이상만
+const vote835 = () => {
     let datas;
-    for (let item of dataSearch) {
-        datas = item.filter((data) => data.vote_average > 8.5)
-    }
+    const vote = document.querySelector('#input').value;
+    datas = MM.dataForShow.filter((data) => data.vote_average >= vote)
+
     console.log(datas);
     let count = 0;
 
@@ -177,55 +292,34 @@ const vote85 = () => {
         item2.makeCard(count);
         count++;
     });
-    awake();
-
+    giveCardEvent();
 }
 
-//api 받기
-const getData = async function (pages = 1) {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${pages}`, options)
-    const data = await response.json();
-    const movieDatasArr = [];
-    const md = {};
 
-    for (const movie of data['results']) {
-        const temp = new movieDatas(movie['title'], movie['overview'], movie['poster_path'], movie['vote_average'], movie['id'])
-        movieDatasArr.push(temp);
+
+//한 페이지당 보이는 갯수
+const changeQuantity = count => {
+    if (count === 1) {
+        document.querySelector('#cards').innerHTML = '';
+        MM.currPage -= (MM.quantity - 1);
+        MM.quantity = 1;
+        print(MM.currPage);
+        console.log("1");
     }
-    dataSearch.push(movieDatasArr);
-    return movieDatasArr;
-}
-
-//출력
-const print = async (pages) => {
-    let data = [];
-    for (let i = 0; i < 1; i++) {
-        data.push(await getData(i + 1));
+    if (count === 2) {
+        document.querySelector('#cards').innerHTML = '';
+        MM.currPage -= (MM.quantity - 1);
+        MM.quantity = 2;
+        print(MM.currPage);
+        console.log("2");
     }
-    count = 0;
-    dataSearch[pages];
-    data.forEach(item => {
-        item.forEach(item2 => {
-            item2.makeCard(count);
-            count++;
-        })
-    });
-
-    await awake();
-    return data;
-}
-
-const awake = async () => {
-    for (let i = 0; i < document.getElementById('cards').childElementCount; i++) {
-        document.getElementById(`cardbutton${i}`).addEventListener('click', function (event) {
-            alert(`영화 id : ${dataSearch[Math.trunc(i / 20)][i % 20]['id']}`)
-        })
+    if (count === 4) {
+        document.querySelector('#cards').innerHTML = '';
+        MM.currPage -= (MM.quantity - 1);
+        MM.quantity = 4;
+        print(MM.currPage);
+        console.log("4");
     }
-    document.getElementById("input").focus();
-    document.getElementById("input").addEventListener('keydown', event => {
-        if (event.code == 'Enter')
-            search();
-    });
 }
 
-print();
+print(1, MM.quantity);
